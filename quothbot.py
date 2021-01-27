e284d3396e7dac4c038f60e3e4ea17f88b2c0686
keeeal <james@keal.com.au>
2021-01-27 11:35:17 +1030

    raw reaction events, live updating

--- quothbot.py
+++ quothbot.py
@@ -1,7 +1,7 @@
 
-import os, asyncio
+import os, configparser
+from asyncio import gather
 from itertools import chain
-from configparser import ConfigParser
 from random import choice
 
 import discord
@@ -11,7 +11,7 @@ data = None
 
 # return all messages from a guild
 async def get_messages(guild):
-    return list(chain(*await asyncio.gather(*map(
+    return list(chain(*await gather(*map(
         lambda c: c.history(limit=None).flatten(),
         guild.text_channels
     ))))
@@ -20,18 +20,20 @@ async def get_messages(guild):
 # return a dictionary of guild: messages
 async def get_data(client):
     return dict(zip(client.guilds,
-        await asyncio.gather(*map(get_messages, client.guilds))
+        await gather(*map(get_messages, client.guilds))
     ))
 
 
+# return a message with author and timestamp
 def embed_message(message):
     embed = discord.Embed(
         description=message.content,
-        timestamp=message.created_at
+        timestamp=message.created_at,
     )
     embed.set_author(
         name=message.author.display_name,
-        icon_url=message.author.avatar_url
+        icon_url=message.author.avatar_url,
+        url=message.jump_url,
     )
 
     # if attachments then choose one
@@ -44,7 +46,7 @@ def embed_message(message):
 def main(config_file):
 
     # load config file or create it
-    config = ConfigParser()
+    config = configparser.ConfigParser()
     if os.path.isfile(config_file):
         config.read(config_file)
     else:
@@ -57,12 +59,13 @@ def main(config_file):
 
     # check for auth token
     if not config['quothbot']['token']:
-        raise discord.LoginFailure(f'Empty token in "{config_file}"')
+        raise discord.LoginFailure(f'No token in "{config_file}"')
 
     # create client
     client = discord.Client()
 
 
+    # initial server scrape
     @client.event
     async def on_ready():
         global data
@@ -72,11 +75,20 @@ def main(config_file):
         print('Ready to quoth')
 
 
+    # live updating
     @client.event
-    async def on_reaction_add(reaction, user):
-        channel = reaction.message.channel
-        if reaction.emoji != '🐦':
+    async def on_message(message):
+        if data:
+            data[message.guild].append(message)
+
+
+    # respond
+    @client.event
+    async def on_raw_reaction_add(event):
+        if event.emoji.name != '🐦':
             return
+        
+        channel = client.get_channel(event.channel_id)
 
         # report no data
         if not data:
@@ -100,5 +112,5 @@ def main(config_file):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config-file', '-c', default='config.ini')
+    parser.add_argument('-c', '--config-file', default='config.ini')
     main(**vars(parser.parse_args()))
